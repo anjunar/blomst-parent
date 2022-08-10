1da82342ad552fbbcca37c20bf822123624eafce
Patrick Bittner <anjunar@gmx.de>
2022-08-10 23:04:23 +0200

    rewrite filter with opcsoft

--- application/src/main/java/com/anjunar/blomst/Oracle18Dialect.java
+++ application/src/main/java/com/anjunar/blomst/Oracle18Dialect.java
@@ -1,13 +1,33 @@
 package com.anjunar.blomst;
 
 
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.hibernate.dialect.Oracle12cDialect;
+import org.hibernate.query.spi.QueryEngine;
+import org.hibernate.query.sqm.function.SqmFunctionRegistry;
+import org.hibernate.type.StandardBasicTypes;
+import org.hibernate.type.spi.TypeConfiguration;
 
 public class Oracle18Dialect extends Oracle12cDialect {
 
-    public Oracle18Dialect() {
-//        registerFunction("levenshtein", new OracleSQLLevenstheinFunction());
-//        registerFunction("contains", new OracleSQLContainsFunction());
-    }
+    @Override
+    public void initializeFunctionRegistry(QueryEngine queryEngine) {
+        super.initializeFunctionRegistry(queryEngine);
 
+        TypeConfiguration typeConfiguration = queryEngine.getTypeConfiguration();
+        SqmFunctionRegistry functionRegistry = queryEngine.getSqmFunctionRegistry();
+
+        functionRegistry.registerPattern(
+                "levensthein",
+                "UTL_MATCH.EDIT_DISTANCE(?1, ?2)",
+                typeConfiguration.getBasicTypeRegistry().resolve(StandardBasicTypes.INTEGER)
+        );
+
+        functionRegistry.registerPattern(
+                "contains",
+                "contains(?1, ?2, 1)",
+                typeConfiguration.getBasicTypeRegistry().resolve(StandardBasicTypes.INTEGER)
+        );
+
+    }
 }
---
+++ application/src/main/java/com/anjunar/blomst/RewriteConfigurationProvider.java
@@ -0,0 +1,29 @@
+package com.anjunar.blomst;
+
+import jakarta.servlet.ServletContext;
+import org.ocpsoft.rewrite.annotation.RewriteConfiguration;
+import org.ocpsoft.rewrite.config.Configuration;
+import org.ocpsoft.rewrite.config.ConfigurationBuilder;
+import org.ocpsoft.rewrite.config.Direction;
+import org.ocpsoft.rewrite.servlet.config.Forward;
+import org.ocpsoft.rewrite.servlet.config.HttpConfigurationProvider;
+import org.ocpsoft.rewrite.servlet.config.Path;
+
+@RewriteConfiguration
+public class RewriteConfigurationProvider extends HttpConfigurationProvider
+{
+    @Override
+    public int priority()
+    {
+        return 10;
+    }
+
+    @Override
+    public Configuration getConfiguration(final ServletContext context)
+    {
+        return ConfigurationBuilder.begin()
+                .addRule()
+                .when(Direction.isInbound().and(Path.matches("/navigator")))
+                .perform(Forward.to("/"));
+    }
+}
